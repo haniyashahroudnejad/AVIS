c27bd2d431d6ce1fcb7864960802ac19545ced5a
unknown <haniya.shahroudnejad@gmail.com>
2024-07-21 23:56:10 +0330

    first commit

---
+++ example.py
@@ -0,0 +1,90 @@
+'''
+@ 2023, Copyright AVIS Engine
+- An Example Compatible with AVISEngine version 2.0.1 / 1.2.4 (ACL Branch) or higher
+''' 
+import avisengine
+import config
+import time
+import cv2
+from imageProccess import *
+
+
+
+# Creating an instance of the Car class
+car = avisengine.Car()
+
+# Connecting to the server (Simulator)
+car.connect(config.SIMULATOR_IP, config.SIMULATOR_PORT)
+
+# Counter variable
+counter = 0
+Steering=0
+debug_mode = False
+
+# Sleep for 3 seconds to make sure that client connected to the simulator 
+time.sleep(3)
+
+try:
+    while(True):
+        # Counting the loops
+        counter = counter + 1
+
+        # Set the power of the engine the car to 20, Negative number for reverse move, Range [-100,100]
+        car.setSpeed(20)
+
+        # Set the Steering of the car -10 degree from center, results the car to steer to the left
+       
+        car.setSteering(Steering)
+        
+        # Set the angle between sensor rays to 45 degrees, Use this only if you want to set it from python client
+        # Notice: Once it is set from the client, it cannot be changed using the GUI
+        car.setSensorAngle(45) 
+
+        # Get the data. Need to call it every time getting image and sensor data
+        car.getData()
+
+        # Start getting image and sensor data after 4 loops
+        if(counter > 4):
+            # Returns a list with three items which the 1st one is Left sensor data\
+            # the 2nd one is the Middle Sensor data, and the 3rd is the Right one.
+            sensors = car.getSensors() 
+            left_sensor = sensors[0]
+            middle_sensor = sensors[1]
+            right_sensor = sensors[2]
+           
+            
+            # Returns an opencv image type array. if you use PIL you need to invert the color channels.
+            image = car.getImage()
+            if(not image is None):
+        
+                # Returns an integer which is the real time car speed in KMH
+                carSpeed = car.getSpeed()
+                if(debug_mode):
+                    (f"Speed : {carSpeed}") 
+                    (f'Left : {str(sensors[0])} | Middle : {str(sensors[1])} | Right : {str(sensors[2])}')
+
+               
+                if((right_sensor <600) or middle_sensor<1000 ):
+                    frame, steering_deg =  image_processor(image,400)
+                    car.setSpeed(15)
+                    Steering-=70
+                    print("Steering",Steering)
+                    car.setSteering(Steering)  
+                elif((left_sensor <600) or middle_sensor<1000 ):   
+                        frame, steering_deg =  image_processor(image,113)
+                        car.setSteering(290)  
+                else :
+                    Steering=0
+                    frame, steering_deg =  image_processor(image,113)
+                    car.setSteering(steering_deg)    
+
+                # Showing the opencv type image
+                cv2.imshow('frame',frame)
+                if cv2.waitKey(10) == ord('q'):
+                    break
+            time.sleep(0.001)
+
+finally:
+    car.stop()
+   
+   
---
+++ imageProccess.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+
+
+def canny(image):
+    gray = cv2.cvtColor(image , cv2.COLOR_RGB2GRAY)
+    gaussian = cv2.GaussianBlur(gray,(5,5),0)
+    canny = cv2.Canny(gaussian,50,150,apertureSize=3)
+    return canny
+
+def croppImg(image , mask):
+    return cv2.bitwise_and(image,mask)
+
+def maskImage(image):
+    height = image.shape[0]
+    triangle = np.array([
+	[(45,395),(height,395),(265,150)]
+	])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,triangle,255)
+    maskImage = croppImg (image, mask)
+    return maskImage   
+
+def displayLines(image, lines):
+    line_image = np.zeros_like(image)
+    
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (0, 0, 255), 5)
+           
+    return line_image
+
+    
+def houghLines(canny):
+    lines=cv2.HoughLinesP(maskImage(canny),1,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+    return lines
+   
+def position_detect(detected_line):
+	POSITION = np.mean(np.where(detected_line > 0),axis = 1)
+	return POSITION
+
+
+def error_detect(POSITION,avg):
+    error = avg - POSITION[1]
+    print("error=",error)
+    return error
+
+def calculate_steering(error):
+	# Error < 0 --> turn right
+	# Error > 0 --> turn left
+    if(error>90 ) : change = 1
+    elif ( error<-50 ) : change = 1.2
+    else : change=0.7
+    steering_degree = -1 * error * change
+    return steering_degree
+
+def image_processor(image,avg):
+    cann = canny(image) 
+    line_image = displayLines(image,houghLines(cann))
+    combo_image = cv2.addWeighted(image,0.8,line_image,1,1)
+    up = np.array([129,255,255])
+    down = np.array([103, 200, 200])
+    line_selection = cv2.inRange(combo_image, down, up)
+    POSITION = position_detect(line_selection)
+    error = error_detect(POSITION,avg)
+    steering_degree = calculate_steering(error)
+    return combo_image,steering_degree    
+
+def colorMask(image , high , low):
+    #img to hsv img
+    hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    
+    #find yellow color
+    mask = cv2.inRange(hsv, np.array(low), np.array(high))
+    return mask    
+        
+def image_processed_middle_line_getter(image):
+    yellowMask = colorMask(image , [45, 255, 255] , [22, 93, 0] )
+    line_image = houghLines(yellowMask)
+    return line_image
