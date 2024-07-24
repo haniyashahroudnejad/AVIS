9a36045dcdb8269d3af76951a8346aeb8b31596a
thisisvesal <vesal.ea84@gmail.com>
2024-07-24 20:57:12 +0330

    Bird's eye view initial

--- avisengine.py
+++ avisengine.py
@@ -17,7 +17,7 @@ __email__ = "amir@avisengine.com"
 class Car():
     '''
     AVIS Engine Main Car class
-    
+
     Attributes
     ----------
 
@@ -36,33 +36,35 @@ class Car():
         sensor_angle
     '''
 
-    # Attributes to kind of replicate a Pub-sub pattern messaging to request data  
+    # Attributes to kind of replicate a Pub-sub pattern messaging to request data
     steering_value = 0
     speed_value = 0
     sensor_status = 1
     image_mode = 1
     get_Speed = 1
     sensor_angle = 30
-    
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Data format for request
-    data_arr = [speed_value, steering_value, image_mode, sensor_status, get_Speed, sensor_angle]
+    data_arr = [speed_value, steering_value, image_mode,
+                sensor_status, get_Speed, sensor_angle]
     _data_format = "Speed:{},Steering:{},ImageStatus:{},SensorStatus:{},GetSpeed:{},SensorAngle:{}"
-    data_str = _data_format.format(data_arr[0], data_arr[1], data_arr[2], data_arr[3], data_arr[4], data_arr[5])
-    
+    data_str = _data_format.format(
+        data_arr[0], data_arr[1], data_arr[2], data_arr[3], data_arr[4], data_arr[5])
+
     image = None
     sensors = None
     current_speed = None
 
-    def connect(self,server,port):
+    def connect(self, server, port):
         '''
         Connecting to the simulator (server)
         '''
         try:
             self.sock.connect((server, port))
             self.sock.settimeout(5.0)
-            
+
             print("connected to ", server, port)
             return True
         except:
@@ -79,15 +81,15 @@ class Car():
             part = socket.recv(BUFFER_SIZE).decode("utf-8")
             data += part
             # Use KMP search to find the <EOF>, KMPSearch() returns -1 if the pattern was not found
-            if(utils.KMPSearch("<EOF>", data) > -1):
+            if (utils.KMPSearch("<EOF>", data) > -1):
                 break
-            
+
         return data
-        
-    def setSteering(self,steering):
+
+    def setSteering(self, steering):
         '''
         Setting the steering of the car
-        
+
         Parameters
         ----------
             steering : int
@@ -100,10 +102,10 @@ class Car():
         self.sock.sendall(self.data_str.encode("utf-8"))
         time.sleep(0.01)
 
-    def setSpeed(self,speed):
+    def setSpeed(self, speed):
         '''
         Setting the speed of the car
-        
+
         Parameters
         ----------
             speed : int
@@ -118,7 +120,7 @@ class Car():
     def setSensorAngle(self, angle):
         '''
         Setting the angle between each sensor ray
-        
+
         Parameters
         ----------
             angle : int
@@ -130,7 +132,7 @@ class Car():
         self.sensor_angle = angle
         self.updateData()
         self.sock.sendall(self.data_str.encode("utf-8"))
-        
+
     def getData(self):
         '''
         Requesting for the data from the simulator
@@ -144,25 +146,26 @@ class Car():
 
         imageTagCheck = re.search('<image>(.*?)<\/image>', receive)
         sensorTagCheck = re.search('<sensor>(.*?)<\/sensor>', receive)
-        speedTagCheck = re.search('<speed>(.*?)<\/speed>', receive)            
-        
+        speedTagCheck = re.search('<speed>(.*?)<\/speed>', receive)
+
         try:
-            if(imageTagCheck):
+            if (imageTagCheck):
                 imageData = imageTagCheck.group(1)
                 im_bytes = base64.b64decode(imageData)
-                im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  # im_arr is one-dim Numpy array
+                # im_arr is one-dim Numpy array
+                im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
                 imageOpenCV = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
                 self.image = imageOpenCV
-            
-            if(sensorTagCheck):
+
+            if (sensorTagCheck):
                 sensorData = sensorTagCheck.group(1)
                 sensor_arr = re.findall("\d+", sensorData)
-                sensor_int_arr = list(map(int, sensor_arr)) 
+                sensor_int_arr = list(map(int, sensor_arr))
                 self.sensors = sensor_int_arr
             else:
-                self.sensors = [1500,1500,1500]
+                self.sensors = [1500, 1500, 1500]
 
-            if(speedTagCheck):
+            if (speedTagCheck):
                 current_sp = speedTagCheck.group(1)
                 self.current_speed = int(current_sp)
             else:
@@ -184,20 +187,22 @@ class Car():
                 [Left Sensor: int, Middle Sensor: int, Right Sensor: int]
         '''
         return self.sensors
-    
+
     def getSpeed(self):
         '''
         Returns the speed of the car
         '''
         return self.current_speed
-    
+
     def updateData(self):
         '''
         Updating the request data array and data string
         '''
-        data = [self.speed_value,self.steering_value,self.image_mode,self.sensor_status,self.get_Speed, self.sensor_angle]
-        self.data_str = self._data_format.format(data[0], data[1], data[2], data[3], data[4], data[5])
-        
+        data = [self.speed_value, self.steering_value, self.image_mode,
+                self.sensor_status, self.get_Speed, self.sensor_angle]
+        self.data_str = self._data_format.format(
+            data[0], data[1], data[2], data[3], data[4], data[5])
+
     def stop(self):
         '''
         Stoping the car and closing the socket
@@ -207,9 +212,6 @@ class Car():
         self.sock.sendall("stop".encode("utf-8"))
         self.sock.close()
         print("Process stopped successfully!")
-    
+
     def __del__(self):
         self.stop()
-    
-        
-        
--- imageProccess.py
+++ imageProccess.py
@@ -3,79 +3,117 @@ import numpy as np
 
 
 def canny(image):
-    gray = cv2.cvtColor(image , cv2.COLOR_RGB2GRAY)
-    gaussian = cv2.GaussianBlur(gray,(5,5),0)
-    canny = cv2.Canny(gaussian,50,150,apertureSize=3)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    gaussian = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(gaussian, 50, 150, apertureSize=3)
     return canny
 
-def croppImg(image , mask):
-    return cv2.bitwise_and(image,mask)
+
+def croppImg(image, mask):
+    return cv2.bitwise_and(image, mask)
+
 
 def maskImage(image):
     height = image.shape[0]
     triangle = np.array([
-	[(45,395),(height,395),(265,150)]
-	])
+        [(45, 395), (height, 395), (265, 150)]
+    ])
     mask = np.zeros_like(image)
-    cv2.fillPoly(mask,triangle,255)
-    maskImage = croppImg (image, mask)
-    return maskImage   
+    cv2.fillPoly(mask, triangle, 255)
+    maskImage = croppImg(image, mask)
+    return maskImage
+
 
 def displayLines(image, lines):
     line_image = np.zeros_like(image)
-    
+
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image, (x1, y1), (x2, y2), (0, 0, 255), 5)
-           
+
     return line_image
 
-    
+
 def houghLines(canny):
-    lines=cv2.HoughLinesP(maskImage(canny),1,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+    lines = cv2.HoughLinesP(maskImage(canny), 1, np.pi/180,
+                            100, np.array([]), minLineLength=40, maxLineGap=5)
     return lines
-   
+
+
 def position_detect(detected_line):
-	POSITION = np.mean(np.where(detected_line > 0),axis = 1)
-	return POSITION
+    POSITION = np.mean(np.where(detected_line > 0), axis=1)
+    return POSITION
 
 
-def error_detect(POSITION,avg):
+def error_detect(POSITION, avg):
     error = avg - POSITION[1]
-    print("error=",error)
+    print("error=", error)
     return error
 
+
 def calculate_steering(error):
-	# Error < 0 --> turn right
-	# Error > 0 --> turn left
-    if(error>90 ) : change = 1
-    elif ( error<-50 ) : change = 1.2
-    else : change=0.7
+    # Error < 0 --> turn right
+    # Error > 0 --> turn left
+    if (error > 90):
+        change = 1
+    elif (error < -50):
+        change = 1.2
+    else:
+        change = 0.7
     steering_degree = -1 * error * change
     return steering_degree
 
-def image_processor(image,avg):
-    cann = canny(image) 
-    line_image = displayLines(image,houghLines(cann))
-    combo_image = cv2.addWeighted(image,0.8,line_image,1,1)
-    up = np.array([129,255,255])
+
+def image_processor(image, avg):
+    cann = canny(image)
+    line_image = displayLines(image, houghLines(cann))
+    combo_image = cv2.addWeighted(image, 0.8, line_image, 1, 1)
+    up = np.array([129, 255, 255])
     down = np.array([103, 200, 200])
     line_selection = cv2.inRange(combo_image, down, up)
     POSITION = position_detect(line_selection)
-    error = error_detect(POSITION,avg)
+    error = error_detect(POSITION, avg)
     steering_degree = calculate_steering(error)
-    return combo_image,steering_degree    
+    return combo_image, steering_degree
 
-def colorMask(image , high , low):
-    #img to hsv img
-    hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    
-    #find yellow color
+
+def colorMask(image, high, low):
+    # img to hsv img
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # find yellow color
     mask = cv2.inRange(hsv, np.array(low), np.array(high))
-    return mask    
-        
+    return mask
+
+
 def image_processed_middle_line_getter(image):
-    yellowMask = colorMask(image , [45, 255, 255] , [22, 93, 0] )
+    yellowMask = colorMask(image, [45, 255, 255], [22, 93, 0])
     line_image = houghLines(yellowMask)
     return line_image
+
+
+def bird_eye_view(frame):
+    height = 550
+    width = 550  # 512
+    # Define source points (corners of the trapezoid)
+    src = np.float32([
+        [20, 260],
+        [480, 260],
+        [480, 350],
+        [20, 350]
+    ])
+    # Define destination points (corners of the rectangle)
+    dst = np.float32([
+        [0, 0],
+        [width, 0],
+        [width, height],
+        [0, height]
+    ])
+
+    # Get the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+
+    # Warp the image to get the bird's eye view
+    bird_eye = cv2.warpPerspective(frame, M, (width, height))
+    cv2.imshow("bird_eye", bird_eye)
